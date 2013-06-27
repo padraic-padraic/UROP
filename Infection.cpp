43a1194daf14e513e432f78dbb109f7309c35a41
padraic-padraic <padraic.calpin93@gmail.com>
2013-06-27 23:00:19 +0100

    Corrected Initial Simulation & Build Script, added initial results

--- Infection.cpp
+++ Infection.cpp
@@ -1,25 +1,27 @@
-#include <Randomlib/Random.hpp>
+#include <RandomLib/Random.hpp>
 #include <iostream>
 #include <fstream>
 #include <cmath>
 using namespace std;
 int main() {
 	RandomLib::Random r; //Random number object from the Random Lib implementation of SFMT Algorithm
-	r.ReSeed(); //Seeding implementation from Random Lib
+	r.Reseed(); //Seeding implementation from Random Lib
 	int time;
-	; /* Generates integer in range 0-10 
+	int infected = r.IntegerC(0,10); /* Generates integer in range 0-10 
 	This line defines variables and gives us a small initial infected population */
-	int healthy = 100000 - infected; // Population of 100000
-	float prob, norm = infected / 1000000; // Normalise between 0 and 1
+	int healthy = 1000 - infected; // Population of 100000
+	float prob, norm = infected / 10000; // Normalise between 0 and 1
 	ofstream out ("Stochastic.txt");
+	cout << time << "\t" << healthy << "\t" << infected << endl;
 	out << "Time step" << "\t" << "Healthy" << "\t" << "Infected" << endl;
-	for (time; time < 20000; time++) {
+	for (time, infected; time < 10000 && infected < 1000; time++) {
+		cout << time << endl;
 		prob = norm*(1.0 - norm);
-		for (int i = 0; i < 100000; i++)
-			if (r.floatN() > prob) {
+			if (r.FloatN() > prob) {
 				infected++;
 				healthy--;
+				cout << "Infection!" << infected << endl;	
 			}
 		out << time << "\t" << healthy << "\t" << infected << endl;
-	}
+}
 }
--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 echo "Moving in."
 cd /home/padraic/Projects/UROP
 echo "Cleaning up"
-rm ./MTTest
-rm ./MTTest.o
+rm ./Infection
+rm ./Infection.o
 echo "Step 1"
-g++ -c -g -O3 MTTest.cpp -I /usr/local/include
+g++ -c -g -O3 ./Infection.cpp -I /usr/local/include
 echo "Step 2"
-g++ -g -o MTTest MTTest.o -L /usr/local/lib -l Random
+g++ -g -o Infection Infection.o -L /usr/local/lib -l Random
 echo "Step 3"
-g++ -g -o MTTest MTTest.o -Wl,-rpath=/usr/local/lib -L/usr/local/lib -lRandom
+g++ -g -o Infection Infection.o -Wl,-rpath=/usr/local/lib -L/usr/local/lib -lRandom
 echo "Built!"
