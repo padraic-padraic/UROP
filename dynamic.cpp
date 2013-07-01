02775a7033e685808584ca326f4534808959069e
padraic-padraic <padraic.calpin93@gmail.com>
2013-07-01 13:02:32 +0100

    Cleaned up failed simulation, added dynamic code

--- dynamic.cpp
+++ dynamic.cpp
@@ -3,5 +3,15 @@
 #include <fstream>
 using namespace std;
 int main() {
-	float x, r, 
+	double x_n, r, x_n1;
+	r = 3.3;
+	for (r; r < 4.1; r += 0.1) {
+		ofstream outifle(r+".txt");
+		for (int i = 0; i < 100000; i++) {
+			outfile << i << "\t" << x_n << endl;
+			x_n = 0.1;
+			x_n1 = r*x_n*(1-x_n);
+			x_n = x_n1;
+		}
+	}
 }
