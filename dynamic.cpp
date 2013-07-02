532ac01ddfa2bceff974daa67a64004d8abccccc
padraic-padraic <padraic.calpin93@gmail.com>
2013-07-02 13:02:49 +0100

    Fixed amatureish mistake

--- dynamic.cpp
+++ dynamic.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include <string>
 using namespace std;
 int main() {
-	double x_n, r, x_n1;
-	r = 3.3;
-	for (r; r < 4.1; r += 0.1) {
-		ofstream outifle(r+".txt");
-		for (int i = 0; i < 100000; i++) {
-			outfile << i << "\t" << x_n << endl;
-			x_n = 0.1;
-			x_n1 = r*x_n*(1-x_n);
-			x_n = x_n1;
-		}
+	double x_n = 0.1, r, x_n1;
+	r = 3.95;
+	ofstream outfile("3.95.txt");
+	for (int i = 0; i < 100000; i++) {
+		outfile << i << "\t" << x_n << endl;
+		x_n1 = r*x_n*(1-x_n);
+		x_n = x_n1;
 	}
 }
