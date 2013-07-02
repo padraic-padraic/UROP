647645f958a9c072d0954fb41dfc9d05ea3a5646
padraic-padraic <padraic.calpin93@gmail.com>
2013-07-02 15:42:19 +0100

    Added Mean calculations in executable way, removed excess executions

--- stochastic.cpp
+++ stochastic.cpp
@@ -8,15 +8,13 @@ int main() {
 	double g = 3.6, x_n, x_n1, sum = 0, sum2 = 0, t = 0;
 	r.Reseed();
 	x_n = r.IntegerC(1, 10) / 1000;
-	fstream outfile("Stoch3.6.txt");
+	ofstream outfile("Stoch3.6.txt");
 	for (t; t < 20000; t++) {
-		for (int i = 0; i < 1000; i++){
-			if (r.FixedN() > g*x_n) {
-				x_n += 0.001;
-			}
-			if (r.FixedN() < g*(x_n*x_n)) {
-				x_n -= 0.001;
-			}
+		if (r.FixedN() > g*x_n) {
+			x_n += 0.001;
+		}
+		if (r.FixedN() < g*(x_n*x_n)) {
+			x_n -= 0.001;
 		}
 		outfile << t << "\t" << x_n << endl;
 		sum += x_n;
@@ -27,4 +25,23 @@ int main() {
 	cout << "The mean is " << m1 << endl;
 	cout << "The Variance is " << m2 - (m1*m1) << endl;
 	cout << "The value of g is " << g << endl;
+	cout << "Mean calculations...." << endl;
+	sum = sum2 = 0;
+	ofstream outfile2("Stochmean.txt");
+	for (g = 0.1; g < 4.1; g += 0.1) {
+		for (int i = 0; i < 1000; i++){
+			if (r.FixedN() > g*x_n) {
+				x_n += 0.001;
+			}
+			if (r.FixedN() < g*(x_n*x_n)) {
+				x_n -= 0.001;
+			}
+			sum += x_n;
+			sum2 += (x_n*x_n);
+		}
+		m1 = sum / (1001);
+		m2 = sum / (1001);
+		outfile2 << g << "\t" << m1 << "\t" << m2 - (m1*m1) << endl;
+		//cout << g << endl;
+	}
 }
