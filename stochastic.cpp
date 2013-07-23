02b5a6c8ab802b5739a445f8bde97619439be661
padraic-padraic <padraic.calpin93@gmail.com>
2013-07-23 14:28:56 +0100

    Undoing all my work,  Yay

--- stochastic.cpp
+++ stochastic.cpp
@@ -7,13 +7,13 @@ int main() {
 	RandomLib::Random r;
 	double g = 3.57, x_n, x_n1, sum = 0, sum2 = 0, t = 0, random;
 	r.Reseed();
-	x_n = r.Fixed() / 2;
+	x_n = 0.2;
 	ofstream outfile("Stoch3.57.txt");
 	for (t; t < 20000; t++) {
 		if (r.FixedN()< g*x_n) {
 			x_n += 0.001;
 		}
-		if (r.FixedN() < g*(1 - (x_n*x_n))) {
+		if (r.FixedN() < g*(x_n*x_n)) {
 			x_n -= 0.001;
 		}
 		outfile << t << "\t" << x_n << endl;
@@ -28,7 +28,7 @@ int main() {
 	cout << "Mean calculations...." << endl;
 	ofstream outfile2("Stochmean.txt");
 	for (g = 0.1; g < 4.5; g += 0.01) {
-		x_n = r.FixedN() / 100; 
+		x_n = 0.1; 
 		//cout << x_n << endl;
 		sum = 0;
 		sum2 = 0;
@@ -36,7 +36,7 @@ int main() {
 			if (r.FixedN() < g*x_n) {
 				x_n += 0.001;
 			}
-			if (r.FixedN() < g*(1 - (x_n*x_n))) { // Should test random or Random squared? Same or different random number?
+			if (r.FixedN() < g*(x_n*x_n)) { // Should test random or Random squared? Same or different random number?
 				x_n -= 0.001;
 			}
 			sum += x_n;
