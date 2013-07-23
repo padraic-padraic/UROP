a9424508c14ce25e39aa0e4ce096836af988926b
padraic-padraic <padraic.calpin93@gmail.com>
2013-07-23 13:41:07 +0100

    Fixed Integer rounding bug leading to negative values

--- stochastic.cpp
+++ stochastic.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 int main() {
 	RandomLib::Random r;
-	double g = 3.75, x_n, x_n1, sum = 0, sum2 = 0, t = 0, random;
+	double g = 3.57, x_n, x_n1, sum = 0, sum2 = 0, t = 0, random;
 	r.Reseed();
-	x_n = r.IntegerC(1, 100) / 100;
-	ofstream outfile("Stoch3.75.txt");
+	x_n = r.Fixed() / 2;
+	ofstream outfile("Stoch3.57.txt");
 	for (t; t < 20000; t++) {
 		if (r.FixedN()< g*x_n) {
 			x_n += 0.001;
